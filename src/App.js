06d7cdab5ae6752c4ead4094f0c1eb464a62e1af
Patrycja Oosthuizen <pielszym@googlemail.com>
2024-06-11 07:47:09 +0100

    Added search engine to project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import logo from "./reading.jpg";
+import logo from "./reading-panda.jpg";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Patrycja Oosthuizen</small>
+        </footer>
+      </div>
     </div>
   );
 }
